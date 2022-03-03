71b81a3418e504351a21035c7834f3bb810e2088
BiAniTy <drayttc@gmail.com>
2022-03-03 15:32:58 -0300

    Add comments and make the code cleaner

--- js/sketchp.js
+++ js/sketchp.js
@@ -1,12 +1,11 @@
-// As soon as the page fully loads, create 16 divs inside the container
-
-// How to make something activate on load?
-
+// As soon as the page fully loads, create a 16x16 grid of divs inside the container
 window.addEventListener("load", generate_grid_items(256));
 
+// Creates a canvas with (num) squares divs
 function generate_grid_items(num) {
     let container = document.querySelector(".container");
 
+    // Creates divs based on the argument number
     for (let i = 0; i < num; i++) {
         let grid_item = document.createElement("div");
         grid_item.classList.toggle("grid_item");
@@ -17,8 +16,11 @@ function generate_grid_items(num) {
     }
 
     let items_per_column = Math.sqrt(num);
+
+    // The container is a square. This determines the sides' size
     let size = 20 * items_per_column;
 
+    // Changes the container size and grid layout based on the argument number
     container.setAttribute(
         "style",
         `grid-template-columns: repeat(${items_per_column}, 1fr); height: ${size}px; width: ${size}px`
@@ -27,6 +29,7 @@ function generate_grid_items(num) {
     draw();
 }
 
+// Attach event listeners and change the style of each div inside the container
 function draw() {
     let grid_items = document.querySelectorAll(".grid_item");
     grid_items = Array.from(grid_items);
@@ -36,6 +39,7 @@ function draw() {
     });
 }
 
+// Changes the color of the divs by adding a class to it
 function change_color(square) {
     square.target.classList.toggle("shine");
 }
